9ccccf6256a3fa2169f22538cd42cfb2551c140d
Hygor-Bueno <hygor_knd.x1@hotmail.com>
2025-06-02 14:33:22 -0300

    refactored store

--- src/Modules/GAPP/Business/Component/Form/Form.tsx
+++ src/Modules/GAPP/Business/Component/Form/Form.tsx
@@ -9,7 +9,7 @@ import { useMyContext } from '../../../../../Context/MainContext';
 
 const Form: React.FC<IFormProps> = ({ data, handleFunction, resetDataStore, resetForm, setData }) => {
 
-  const defaultFunction = (value: string) => {};
+  const defaultFunction = (value: string) => { };
   const { setLoading } = useMyContext();
 
   const [
@@ -41,14 +41,14 @@ const Form: React.FC<IFormProps> = ({ data, handleFunction, resetDataStore, rese
   );
 
 
-  function searchCEP () {
+  function searchCEP() {
     return consultingCEP(data?.zip_code, setData, setLoading)
   }
 
-  async function postStore(obj: any, conn:any = new Connection('18')) {
+  async function postStore(obj: any, conn: any = new Connection('18')) {
     try {
       setLoading(true);
-      const {success} = await conn.post(obj, 'GAPP/Store.php');
+      const { success } = await conn.post(obj, 'GAPP/Store.php');
       success ?
         handleNotification("Sucesso", "Loja salva com sucesso!", "success") :
         handleNotification("Erro", "Loja não foi salva!", "danger");
@@ -61,10 +61,10 @@ const Form: React.FC<IFormProps> = ({ data, handleFunction, resetDataStore, rese
     }
   }
 
-  async function putStore(obj:any, conn: any = new Connection('18')) {
+  async function putStore(obj: any, conn: any = new Connection('18')) {
     try {
       setLoading(true);
-      const {success} = await conn.put(obj, 'GAPP/Store.php');
+      const { success } = await conn.put(obj, 'GAPP/Store.php');
       success ?
         handleNotification("Sucesso", "Loja atualizada com sucesso!", "success") :
         handleNotification("Erro", "Loja não foi atualizada!", "danger");
@@ -76,7 +76,7 @@ const Form: React.FC<IFormProps> = ({ data, handleFunction, resetDataStore, rese
     }
   }
 
-  function formatStoreData (data: any) {
+  function formatStoreData(data: any) {
     return {
       cnpj: data?.cnpj.replace(/[^a-z0-9]/gi, ""),
       name: data?.name,
@@ -95,40 +95,34 @@ const Form: React.FC<IFormProps> = ({ data, handleFunction, resetDataStore, rese
   const editorSendData = async () => {
     try {
       let result;
-      if(isNewStore) {
+      if (isNewStore) {
         result = await postStore(formatStoreData(data));
       } else {
         result = await putStore(formatStoreData(data));
       }
-      if(result) {
-        if(resetDataStore) resetDataStore();
-        if(resetForm) resetForm();
+      if (result) {
+        if (resetDataStore) resetDataStore();
+        if (resetForm) resetForm();
       }
     } catch (error) {
       handleNotification("Error", String(error).toLowerCase(), "danger");
     }
   };
-
   return (
-    <React.Fragment>
-      <div className='col-12 form-control bg-white bg-opacity-75 shadow m-2 w-100 d-flex flex-column justify-content-between form-style-modal'>
-        <CustomForm
-          classRender='w-100'
-          classButton='btn btn-success'
-          className='p-3'
-          notButton={false}
-          fieldsets={filter}
-          />
-        <div className='row'>
-          <div className="d-flex justify-content-center p-2">
-            <button className={`btn btn-success w-100`} onClick={editorSendData}>
-              <i className={`fa-sharp fa-solid ${isNewStore ? 'fa-paper-plane' : 'fa-arrows-rotate'} text-white`}></i>
-            </button>
-          </div>
-        </div>
+    <div className="d-flex h-25 row col-12">
+      <CustomForm
+        classRender='w-100'
+        classButton='btn btn-success'
+        className='row col-12'
+        notButton={false}
+        fieldsets={filter}
+      />
+      <div className='d-flex align-items-center'>
+        <button className={`btn btn-success`} onClick={editorSendData}>
+          <i className={`fa-sharp fa-solid ${isNewStore ? 'fa-paper-plane' : 'fa-arrows-rotate'} text-white`}></i>
+        </button>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
-
 export default Form;
