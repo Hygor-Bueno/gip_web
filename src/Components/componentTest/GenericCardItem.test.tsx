7c0617ca0cd47d7fd068fb39d62e0b324452d3f5
ProgramacaoPgPs <programacaopegpese@gmail.com>
2025-06-03 11:10:14 -0300

    send new change commit test

---
+++ src/Components/componentTest/GenericCardItem.test.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {render, screen, fireEvent} from "@testing-library/react";
+import GenericCardItem from "../GenericCardItem";
+
+describe("GenericCardItem", () => {
+    const mockItem: any = {
+        name: "Teste",
+        age: 30
+    };
+
+    const mockFields = [
+        {label: "Nome", key: "name"},
+        {label: "Idade", key: "age"}
+    ];
+
+    const setup = (props = {}) => {
+        return render(
+            <GenericCardItem 
+                item={mockItem}
+                fields={mockFields}
+                showRecycle
+                showDelete
+                showEdit
+                {...props}
+            />
+        );
+    }
+
+    test("deve renderizar campos corretamente", () => {
+        setup();
+
+        expect(screen.getByText("Nome:")).toBeInTheDocument();
+        expect(screen.getByText("Teste")).toBeInTheDocument();
+        expect(screen.getByText("Idade:")).toBeInTheDocument();
+        expect(screen.getByText("30")).toBeInTheDocument();
+    })
+
+    test("deve chamar o recycle ao clicar no botão de reciclar", () => {
+        const onRecycle = jest.fn();
+        setup({onRecycle});
+
+        const button = screen.getByLabelText("Reciclar");
+        
+    });
+});
+
+
