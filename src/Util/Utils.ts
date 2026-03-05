b6c43ac6483c1d3fb725057e2396b3826262af18
Jonatas Silva dos Santos <jonatas.santos@PegPese.com>
2026-03-05 11:37:01 -0300

    I need removed the comment of code

--- src/Util/Utils.ts
+++ src/Util/Utils.ts
@@ -218,54 +218,8 @@ export async function fetchNodeDataFull(req: iReqConn, headers?: Record<string, 
     }
 };
 
-// Jonatas silva dos santos | data: 03/03/2026
+// Jonatas silva dos santos | data: 05/03/2026
 // - Fiz uma mudança para trabalhar com FormData para envios de arquivos e leitura binaria
-/*export async function fetchDataFull(req: iReqConn) {
-    let result: any = { error: true, message: "Generic Error!" };
-
-    try {
-        const URL = settingUrl(`/Controller/${req.pathFile}?app_id=${req.appId ? req.appId : "18"}&AUTH=${localStorage.tokenGIPP ? localStorage.tokenGIPP : ''}${req.urlComplement ? req.urlComplement : ""}`);
-        let objectReq: any = { method: req.method };
-
-        if (req.params) {
-            if (req.params instanceof FormData) {
-                objectReq.body = req.params;
-            } else {
-                objectReq.body = JSON.stringify(req.params);
-                objectReq.headers = {
-                    "Content-Type": "application/json"
-                };
-            }
-        }
-
-        const response = await fetch(URL, objectReq);
-        
-        // CHECAGEM DE TIPO: Se for um arquivo, baixa. Se for JSON, continua.
-        const contentType = response.headers.get("content-type");
-        if (contentType && !contentType.includes("application/json")) {
-            const blob = await response.blob();
-            const downloadUrl = window.URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.href = downloadUrl;
-            a.download = "";
-            document.body.appendChild(a);
-            a.click();
-            a.remove();
-            return { error: false };
-        }
-
-        const body = await response.json();
-        result = body;        
-        if (body.error) throw new Error(body.message);
-    } catch (messageErr: any) {
-        const translate = new Translator(messageErr.message);
-        const passDefault = messageErr.message.toLowerCase().includes('default password is not permited');
-        checkedExceptionError(messageErr.message, req.exception) && handleNotification(passDefault ? "Atenção!" : "Erro!", translate.getMessagePT(), passDefault ? "info" : "danger");
-    } finally {
-        return result;
-    }
-};*/
-
 export async function fetchDataFull(req: iReqConn) {
     let result: any = { error: true, message: "Generic Error!" };
 
